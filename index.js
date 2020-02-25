43201653c3e8a5a9c734cee45621bf6479b3993d
Majewski <mmajews2@MGC12YJ052JGH5.fbpld77.ford.com>
2020-02-25 10:31:15 -0500

    express and create react app starter

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { Pool } = require("pg");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+const pool = new Pool({
+  connectionString:
+    process.env.DATABASE_URL ||
+    "postgres://mmajews2:419Terry!@localhost:5432/localdb",
+  ssl: true
+});
+express()
+  .use(express.static(path.join(__dirname, "ui/build")))
+
+  .get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "ui/build/index.html"))
+  )
+  .get("/db", async (req, res) => {
+    try {
+      console.log("connection string ", pool.connectionString);
+      const client = await pool.connect();
+      const result = await client.query("SELECT * FROM test_table");
+      const results = { results: result ? result.rows : null };
+      res.json(results);
+      client.release();
+    } catch (err) {
+      console.log(err);
+      res.send("Error" + err);
+    }
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
