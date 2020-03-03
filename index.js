a9cefcf83e013fe070c2c4e8a5bff08ae95036e2
Fordlab1 <fordlab1@MGC12Z314HJV40.fbpld77.ford.com>
2020-03-03 09:23:18 -0500

    In progress firebase

--- index.js
+++ index.js
@@ -1,21 +1,74 @@
-const express = require("express");
-const cors = require("cors");
-const upload = require("multer");
-const { Pool } = require("pg");
-const path = require("path");
+import express from "express";
+import cors from "cors";
+import upload from "multer";
+import firebase from "firebase";
+import "@firebase/storage"; //needed for initialization
+import XMLHttpRequest from "xmlhttprequest";
+global.XMLHttpRequest = XMLHttpRequest.XMLHttpRequest;
+import Pool from "pg";
+import path from "path";
+
+const FILENAME =
+  typeof __filename !== "undefined"
+    ? __filename
+    : (/^ +at (?:file:\/*(?=\/)|)(.*?):\d+:\d+$/m.exec(Error().stack) || "")[1];
+const DIRNAME =
+  typeof __dirname !== "undefined"
+    ? __dirname
+    : FILENAME.replace(/[\/\\].*?$/, "");
+
 const PORT = process.env.PORT || 5000;
-const pool = new Pool({
+const pool = new Pool.Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
 });
+let storage;
+const initFirebase = () => {
+  var firebaseConfig = {
+    apiKey: "AIzaSyAHjWMqYnREX0BjrPZc1GpniN3ykSNuPpg",
+    authDomain: "throwback-thursday-15e79.firebaseapp.com",
+    databaseURL: "https://throwback-thursday-15e79.firebaseio.com",
+    projectId: "throwback-thursday-15e79",
+    storageBucket: "throwback-thursday-15e79.appspot.com",
+    messagingSenderId: "740383031846",
+    appId: "1:740383031846:web:1ff2742fa5d7eb5b6bdce7"
+  };
+  firebase.initializeApp(firebaseConfig);
+
+  storage = firebase.storage();
+};
+initFirebase();
+
 express()
   .use(cors())
-  .use(express.static(path.join(__dirname, "ui/build")))
+  .use(express.static(path.join(DIRNAME, "ui/build")))
   .get("/", (req, res) =>
-    res.sendFile(path.join(__dirname, "ui/build/index.html"))
+    res.sendFile(path.join(DIRNAME, "ui/build/index.html"))
   )
   .post("/uploadImage", upload().single("image"), (req, res) => {
-    console.log(req.file);
+    try {
+      console.log("file", req.file);
+      const storageRef = storage.ref();
+      const imageRef = storageRef.child("images/file.png");
+      const fileArray = new Uint8Array(req.file.buffer);
+      console.log("filearray", fileArray);
+      imageRef
+        .put(fileArray)
+        .then(
+          snapshot => {
+            console.log("hello world", snapshot.downloadURL);
+            return snapshot.downloadURL;
+          },
+          error => {
+            console.log("error", error);
+          }
+        )
+        .catch(url => {
+          console.log("url", url);
+        });
+    } catch (err) {
+      console.log("err", err);
+    }
     res.send("Success");
   })
   .get("/db", async (req, res) => {
@@ -26,8 +79,8 @@ express()
       res.json(results);
       client.release();
     } catch (err) {
-      console.log(err);
+      console.log("error getting db", err);
       res.send("Error" + err);
     }
   })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+  .listen(PORT, () => console.log(`Listening on this port: ${PORT}`));
