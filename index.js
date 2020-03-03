803789f781167c321588c40d464421f383670e27
Majewski <mmajews2@MGC12YJ052JGH5.fbpld77.ford.com>
2020-03-03 11:41:53 -0500

    upload image to new firebase storage, extract config vars

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+global.XMLHttpRequest = require("xhr2");
 const express = require("express");
 const cors = require("cors");
 const upload = require("multer");
+const firebase = require("firebase");
+require("firebase/storage");
 const { Pool } = require("pg");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
@@ -8,6 +11,34 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
 });
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DB_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: "throwback-thursday-b1cf0.appspot.com",
+  messagingSenderId: process.env.SENDER_ID,
+  appId: process.env.APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+function uploadToFirebase(file) {
+  const storageRefForImage = firebase
+    .storage()
+    .ref()
+    .child("images/" + file.originalname);
+
+  storageRefForImage.put(Uint8Array.from(file.buffer)).then(
+    snapshot => {
+      return snapshot.ref.getDownloadURL();
+    },
+    error => {
+      throw new Error("Error uploading image with error", error);
+    }
+  );
+}
 express()
   .use(cors())
   .use(express.static(path.join(__dirname, "ui/build")))
@@ -15,7 +46,14 @@ express()
     res.sendFile(path.join(__dirname, "ui/build/index.html"))
   )
   .post("/uploadImage", upload().single("image"), (req, res) => {
-    console.log(req.file);
+    try {
+      const storedImageURL = uploadToFirebase(req.file);
+      console.log(storedImageURL);
+    } catch (e) {
+      console.log(e);
+      res.status(500);
+      res.send("Failure");
+    }
     res.send("Success");
   })
   .get("/db", async (req, res) => {
