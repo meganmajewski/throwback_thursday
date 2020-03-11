727e6caa0224b4286dffb34d47f599d541a3e455
Majewski <mmajews2@MGC12YJ052JGH5.fbpld77.ford.com>
2020-03-11 16:30:24 -0400

    try and post url to postgres, ssl problem, pushing to heroku
    fix static build
    
    static build
    
    idk
    
    remove build

--- ui/src/pages/Gallery.tsx
+++ ui/src/pages/Gallery.tsx
@@ -1,5 +1,28 @@
 import React from "react";
+import useAxios from "axios-hooks";
 
 export default function Gallery() {
-  return <div>hello from gallery;</div>;
+  const [{ data, loading, error }, uploadImage] = useAxios({
+    baseURL: "http://localhost:5000",
+    url: "/allImages",
+    method: "get"
+  });
+
+  const printAllImages = () => {
+    console.log("data", data);
+    return data.map((src: string, key: string) => {
+      return <img key={key} src={src}></img>;
+    });
+  };
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
+  if (data)
+    return (
+      <div>
+        {printAllImages()}
+        Thanks for submitting your image! {JSON.stringify(data)}
+      </div>
+    );
+
+  // return <div>everything returned {JSON.stringify(data)}</div>;
 }
