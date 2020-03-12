7dc9ac5519d98e5801b03692ebd3bf82578fffa2
Majewski <mmajews2@MGC12YJ052JGH5.fbpld77.ford.com>
2020-03-12 08:49:29 -0400

    remove build folder from ignore

---
+++ ui/build/precache-manifest.21e147ed56e66ebd1f6b943be7f3537a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70565c4c1897064408fbfec2d234e992",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cf19f77ccc98cbfb220",
+    "url": "/static/css/main.ef735230.chunk.css"
+  },
+  {
+    "revision": "2830ba8ee2591c37eadc",
+    "url": "/static/js/2.60551444.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.60551444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cf19f77ccc98cbfb220",
+    "url": "/static/js/main.db85f4d3.chunk.js"
+  },
+  {
+    "revision": "878fb06727b88112731f",
+    "url": "/static/js/runtime-main.1e7b5999.js"
+  },
+  {
+    "revision": "9419036f5d0e0366e5ca4addd1269225",
+    "url": "/static/media/logo.9419036f.png"
+  }
+]);
