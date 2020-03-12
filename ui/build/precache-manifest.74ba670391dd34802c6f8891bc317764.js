8a6dccf7281cac615f6c997da157ca05c666f390
Fordlab1 <fordlab1@MGC12Z314HJV40.fbpld77.ford.com>
2020-03-12 11:41:53 -0400

    re-build

---
+++ ui/build/precache-manifest.74ba670391dd34802c6f8891bc317764.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd5a58a199fd74422b9bcb5ae603734",
+    "url": "/index.html"
+  },
+  {
+    "revision": "622bd410f6fc5f429bb5",
+    "url": "/static/css/main.ef735230.chunk.css"
+  },
+  {
+    "revision": "c5cf091df012a660f3cd",
+    "url": "/static/js/2.cca86cb2.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.cca86cb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "622bd410f6fc5f429bb5",
+    "url": "/static/js/main.1bfb6ac3.chunk.js"
+  },
+  {
+    "revision": "878fb06727b88112731f",
+    "url": "/static/js/runtime-main.1e7b5999.js"
+  },
+  {
+    "revision": "9419036f5d0e0366e5ca4addd1269225",
+    "url": "/static/media/logo.9419036f.png"
+  }
+]);
