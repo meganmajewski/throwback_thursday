f5de863faa4610c49bf2a78e2d1b1fa3604ec3f9
Majewski <mmajews2@MGC12YJ052JGH5.fbpld77.ford.com>
2020-03-13 14:53:37 -0400

    fix upload page test

--- ui/src/__tests__/Upload.test.tsx
+++ ui/src/__tests__/Upload.test.tsx
@@ -3,7 +3,9 @@ import { render, fireEvent, getByTestId, wait } from "@testing-library/react";
 import Upload from "../pages/Upload";
 import { Simulate } from "react-dom/test-utils";
 
-const mockUploadImage = jest.fn();
+const mockUploadImage = jest
+  .fn()
+  .mockImplementation(() => Promise.resolve("we good here"));
 
 jest.mock("axios-hooks", () => () => [{}, mockUploadImage]);
 
@@ -12,9 +14,7 @@ describe("UploadImage", () => {
     mockUploadImage.mockReset();
   });
   it("can select an image and upload will make a request to upload it", async () => {
-    const { container, getByTestId, getByText, getByAltText } = render(
-      <Upload />
-    );
+    const { getByTestId, getByText } = render(<Upload />);
     const file = new File(["(⌐□_□)"], "chucknorris.png", { type: "image/png" });
     const imageInput = getByTestId("upload-image");
     Simulate.change(imageInput, { target: { files: [file] } as any });
