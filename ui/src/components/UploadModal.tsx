224d64a264fab8eedb4d1a74311fe21becea1788
Majewski <mmajews2@MGC12YJ052JGH5.fbpld77.ford.com>
2020-03-26 15:11:23 -0400

    fix chrome centering for upload modal

--- ui/src/components/UploadModal.tsx
+++ ui/src/components/UploadModal.tsx
@@ -24,7 +24,6 @@ const customStyles = {
     left: "50%",
     right: "auto",
     bottom: "auto",
-    marginRight: "-50%",
     transform: "translate(-50%, -50%)",
     backgroundColor: "white",
     border: "0",
