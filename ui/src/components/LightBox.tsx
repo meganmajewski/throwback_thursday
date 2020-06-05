59595bdf4b3bcb4c30b55b8dbd257bda9f0be1b2
Majewski <mmajews2@MGC12YJ052JGH5.fbpld77.ford.com>
2020-06-05 11:22:38 -0400

    add missing lightbox

---
+++ ui/src/components/LightBox.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Modal from "react-modal";
+import closeButton from "../images/close_24px.png";
+
+export function LightBox(props: any) {
+  return (
+    <div id="image-modal">
+      <Modal
+        id="image-modal"
+        isOpen={props.openModal}
+        overlayClassName="black-background"
+        onRequestClose={() => {
+          props.closeModal();
+        }}
+        style={customStyles}
+        contentLabel="Upload Modal"
+      >
+        <h2 className="image-modal-cdsid">{props.cdsid}</h2>
+        <img
+          className="image-modal-image"
+          style={{ maxWidth: "570px" }}
+          src={props.src}
+        />
+      </Modal>
+    </div>
+  );
+}
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "transparent",
+    border: "0",
+    maxWidth: "600px",
+    maxHeight: "90vh",
+    display: "flex",
+    flexDirection: "column" as "column",
+    alignItems: "center",
+  },
+};
